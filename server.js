08c94fb726744bc66af6dbce6177fc5a969255fb
Ken Qiu <kqiu@drmg.ca>
2017-12-13 16:14:47 -0500

    webhook

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
 const app = express();
+var FBBotFramework = require('fb-bot-framework');
 
 // API file for interacting with MongoDB
 const api = require('./server/routes/api');
@@ -16,6 +17,68 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // API location
 app.use('/api', api);
+
+// Initialize 
+var bot = new FBBotFramework({
+    page_token: "EAAOEyBY1Y04BALhBM3RIPlYdDB5tgyFHCHPPpAnswXSrOZBGF45BecWW0RtNA4oChZCWLV82nrJ8Hg3NaZAF16DB6UyQZAifMTGZBZCFj9kzm9RhXMSZC4KxXh1DyiAjqPp6b6NZC0FfYrDbsGvfPrZBIZAlgcGp5X4vBdGgo4pw2ZAMgZDZD",
+    verify_token: "98523020"
+});
+ 
+// Setup Express middleware for /webhook 
+app.use('/webhooks/facebook', bot.middleware());
+
+// Setup listener for incoming messages 
+bot.on('message', function(userId, message){
+    // bot.sendTextMessage(userId, "Echo Message:" + message); 
+    
+    // Send quick replies 
+    var replies = [
+        {
+            "content_type": "text",
+            "title": "Good",
+            "payload": "thumbs_up"
+        },
+        {
+            "content_type": "text",
+            "title": "Bad",
+            "payload": "thumbs_down"
+        }
+    ];
+    bot.sendQuickReplies(userId, message, replies);
+});
+ 
+// Setup listener for quick reply messages 
+bot.on('quickreply', function(userId, payload){
+    bot.sendTextMessage(userId, "payload:" + payload);
+});
+ 
+// Config the Get Started Button and register a callback 
+bot.setGetStartedButton("GET_STARTED");
+bot.on('postback', function(userId, payload){
+ 
+    if (payload == "GET_STARTED") {
+        getStarted(userId);
+    }
+    
+    // Other postback callbacks here 
+    // ... 
+    
+});
+ 
+function getStarted(userId){
+    
+    // Get started process  
+}
+ 
+// Setup listener for attachment 
+bot.on('attachment', function(userId, attachment){
+    
+    // Echo the audio attachment 
+    if (attachment[0].type == "audio") {
+        bot.sendAudioAttachment(userId, attachment[0].payload.url);
+    }
+    
+});
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
