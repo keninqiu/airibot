f3a817e82f065068f89be997ed074181fe15ea19
Ken Qiu <kqiu@drmg.ca>
2017-12-13 16:31:10 -0500

    fix

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ var bot = new FBBotFramework({
 app.use('/webhooks/facebook', bot.middleware());
 
 // Setup listener for incoming messages 
-bot.on('message', function(userId, message){
+bot.on('messages', function(userId, message){
     bot.sendTextMessage(userId, "Echo Message:" + message); 
 
 	fs.writeFile("/tmp/test", "Hey there!", function(err) {
