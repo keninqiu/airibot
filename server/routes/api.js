64ac59afea84b46939837e80f90688883196a646
Ken Qiu <kqiu@drmg.ca>
2017-12-13 15:41:09 -0500

    webhook

--- server/routes/api.js
+++ server/routes/api.js
@@ -25,7 +25,7 @@ router.get('/users', (req, res) => {
 });
 
 router.get('/webhooks/facebook', (req, res) => {
-    res.json(req.query);
+    res.json(req.query.hub.challenge);
 });
 
 router.post('/webhooks/facebook', (req, res) => {
