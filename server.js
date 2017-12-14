448d78ecf0fdaa7fb1e5bb74c2d7a9f3c8d148fa
Ken Qiu <kqiu@drmg.ca>
2017-12-13 21:56:41 -0500

    fix

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ bot.on('message', function(userId, message){
     //bot.sendTextMessage(userId, "Echo Message:" + message); 
    
     // Send quick replies 
-    
+    var messageText = 'You say:' + message ;
     var replies = [
         {
             "content_type": "text",
@@ -46,7 +46,7 @@ bot.on('message', function(userId, message){
             "payload": "thumbs_down"
         }
     ];
-    bot.sendQuickReplies(userId, message, replies);
+    bot.sendQuickReplies(userId, messageText, replies);
     
 });
  
