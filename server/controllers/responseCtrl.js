a8b8a2bf6871fb6882e9bc4da999dff4f15479ad
Ken Qiu <kqiu@drmg.ca>
2017-12-27 13:36:19 -0500

    fix

--- server/controllers/responseCtrl.js
+++ server/controllers/responseCtrl.js
@@ -1,15 +1,25 @@
+var detect = require('../dialogflow/detect');
+
+const projectId = 'airi-b9eae'; //https://dialogflow.com/docs/agents#settings
+const sessionId = 'quickstart-session-id';
+const languageCode = 'en-US';
+
 module.exports = {
   reply : function(userId, message) {
+  	var queries = [message];
+
+  	detect.detectTextIntent(projectId,sessionId,queries, languageCode)；
+
     var messageText = 'Your userId is ' + userId + ',You say:' + message ;
     var quickReplies = [
         {
             "content_type": "text",
-            "title": "Good answer",
+            "title": "Good",
             "payload": "thumbs_up"
         },
         {
             "content_type": "text",
-            "title": "Bad answer",
+            "title": "Bad",
             "payload": "thumbs_down"
         }
     ];
