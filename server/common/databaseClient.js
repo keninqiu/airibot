c42d37eef981429fcb101c5ce22adc950643a769
Ken Qiu <kqiu@drmg.ca>
2018-01-02 16:17:54 -0500

    fix

--- server/common/databaseClient.js
+++ server/common/databaseClient.js
@@ -12,5 +12,20 @@ module.exports = {
         }
     });
     return client;
+  },
+  execSql:async function(sql) {
+    var client = new Client({
+        mysql: {
+            host: config.DB_HOST,
+            port: 3306,
+            database: config.DB_NAME,
+            user: config.DB_USER,
+            password: config.DB_PASS
+        }
+    });
+    await client.startTransaction();
+    var result = await client.executeTransaction(sql, []);
+    await client.stopTransaction();
+    return result;        
   }
 }
